bd308fbb2d9e0ecd455331decd8dd8c301e92d25
lhs0127 <hi9374328@gmail.com>
2021-02-17 11:43:37 +0900

    first commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+#from django.conf.urls.static import static
+#from django.conf import settings
+#static 이미지를 한번에 모으는게 좋다고 하는 거 같은데 오류남
+
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),
+]#+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
