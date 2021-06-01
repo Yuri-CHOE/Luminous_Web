bee02a100391dbbd54c4d6b176a4b4c7aa3a821d
{lgrin02@naver.com} <lgrin02@naver.com>
2021-06-01 23:05:00 +0900

    알람 오류 일부 수정

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+
 ]#+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
  
